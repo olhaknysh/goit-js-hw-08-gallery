4a1725628f1da5012687d2d4466ca2b2aa13c014
olhaknysh <olhaknysh1994@gmail.com>
2021-01-08 18:11:00 +0200

    Gallery final

---
+++ js/gallery.js
@@ -0,0 +1,97 @@
+import defaultExport from "./gallery-items.js";
+
+const galleryRef = document.querySelector(".js-gallery");
+const modalWindowRef = document.querySelector(".js-lightbox");
+const closeModalBtnRef = document.querySelector(
+  "button[data-action='close-lightbox']"
+);
+const imgModalRef = document.querySelector(".lightbox__image");
+
+const createMarkup = function ({ preview, original, description, index }) {
+  const containerRef = document.createElement("li");
+  containerRef.classList.add("gallery__item");
+
+  const linkRef = document.createElement("a");
+  linkRef.classList.add("gallery__link");
+  linkRef.href = original;
+
+  const imageRef = document.createElement("img");
+  imageRef.classList.add("gallery__image");
+  imageRef.src = preview;
+  imageRef.dataset.source = original;
+  imageRef.alt = description;
+  imageRef.dataset.index = index;
+
+  linkRef.appendChild(imageRef);
+  containerRef.appendChild(linkRef);
+
+  return containerRef;
+};
+
+const images = defaultExport.map((image) => {
+  return createMarkup(image);
+});
+
+galleryRef.append(...images);
+
+galleryRef.addEventListener("click", handleImageOpenModal);
+closeModalBtnRef.addEventListener("click", handleCloseModal);
+modalWindowRef.addEventListener("click", handleOverlayClickClose);
+window.addEventListener("keydown", handleEscapeClose);
+window.addEventListener("keydown", handleImageRemote);
+
+function handleImageOpenModal(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName === "IMG") {
+    imgModalRef.src = event.target.dataset.source;
+    imgModalRef.alt = event.target.alt;
+    imgModalRef.dataset.index = event.target.dataset.index;
+
+    handleModalOpen();
+  }
+}
+
+function handleModalOpen() {
+  modalWindowRef.classList.add("is-open");
+}
+
+function handleCloseModal() {
+  modalWindowRef.classList.remove("is-open");
+
+  imgModalRef.src = "";
+}
+
+function handleEscapeClose(event) {
+  if (modalWindowRef.classList.contains("is-open")) {
+    if (event.code === "Escape") {
+      handleCloseModal();
+    }
+  }
+}
+
+function handleOverlayClickClose(event) {
+  const overlayRef = modalWindowRef.querySelector(".lightbox__overlay");
+  if (event.target === overlayRef) {
+    handleCloseModal();
+  }
+}
+
+function handleImageRemote(event) {
+  if (modalWindowRef.classList.contains("is-open")) {
+    if (event.code === "ArrowRight") {
+      let indexImage = Number(imgModalRef.dataset.index);
+      const nextImageSrc = defaultExport[indexImage + 1].original;
+      imgModalRef.src = nextImageSrc;
+      imgModalRef.dataset.index = defaultExport[indexImage + 1].index;
+      console.log("right");
+    }
+    if (event.code === "ArrowLeft") {
+      let indexImage = Number(imgModalRef.dataset.index);
+      const nextImageSrc = defaultExport[indexImage - 1].original;
+      imgModalRef.src = nextImageSrc;
+      imgModalRef.dataset.index = defaultExport[indexImage - 1].index;
+      console.log("left");
+    }
+  }
+}
